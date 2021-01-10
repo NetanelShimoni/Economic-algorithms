028bc8aac01db46f3f0beb411504b99d6d2ab5cf
Netanel Shimoni <57719538+NetanelShimoni@users.noreply.github.com>
2021-01-11 00:01:29 +0200

    Add files via upload

--- M_12/shift.py
+++ M_12/shift.py
@@ -17,7 +17,7 @@ def find_max_matches(compatible: List[List[bool]]):
      for j, value in enumerate(outer):
       if(value==True):
         if(i!= j and j<size_row and i<size_col):
-          if(i<j):
+          #if(i<j):
            if(compatible[j][i]==True):
               index_1=i;
               index_2=j;
@@ -28,8 +28,6 @@ def find_max_matches(compatible: List[List[bool]]):
               for k in range(j,size_row):
                 if (flag_j==True):
                     temp=0
-
-
                 for t in range(temp,size_col):
                  if(t==(size_col-1)):
                     flag_j=True
@@ -65,7 +63,7 @@ find_max_matches(arr)
 ######Length 2 cycle######
 barr = [[False,True,True]
       ,[True,False,False]
-      ,[False,True,True]
+      ,[False,False,True]
       ,[False,False,False]]
 find_max_matches(barr)
 
